db6ee223d6c3f1217e254b138cb2d606c71a4c28
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2014-08-14 17:38:25 +0100

    Added building_name and sub_building_name to API responses.

--- addressfinder/apps/address/serializers.py
+++ addressfinder/apps/address/serializers.py
@@ -6,5 +6,6 @@ from rest_framework import serializers
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = ('uprn', 'building_number', 'thoroughfare_name',
+        fields = ('uprn', 'building_name', 'sub_building_name',
+                  'building_number', 'thoroughfare_name',
                   'dependent_locality', 'post_town', 'postcode')
