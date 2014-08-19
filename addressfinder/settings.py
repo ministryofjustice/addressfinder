d6a6be944094c2b73288a6d5070847c84a8801d7
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2014-08-19 17:33:26 +0100

    Added token-based authentication

--- addressfinder/settings.py
+++ addressfinder/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.gis',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'rest_framework_gis',
 
     'address'
@@ -97,6 +98,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# REST_FRAMEWORK = {
-#     'PAGINATE_BY': 500,
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
